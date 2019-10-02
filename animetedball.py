9d8b1181de6c6c3972f70878225adc493d00773c
DeminAlex19 <55479297+DeminAlex19@users.noreply.github.com>
2019-10-02 11:24:14 +0300

    Add files via upload

---
+++ animetedball.py
@@ -0,0 +1,60 @@
+from graph import*
+import random
+windowSize(800, 800)
+canvasSize(800, 800)
+ballr = 20
+ballcolor = "black"
+ballvelx = random.randint(0, 10)/10.0
+ballvely = 0
+
+hs = 5
+hr = 495
+
+ballx = random.randint(ballr + hs, hr - ballr)
+bally = random.randint(ballr + hs, hr - ballr)
+
+def changecolor():
+    global ballcolor
+    ballcolor = randColor()
+
+def drawwall():
+    brushColor("white")
+    penColor("black")
+    penSize(5)
+    rectangle(hs-5,hs-5,hr+5,hr+5)
+
+def drawball():
+    global ballx, bally, ballcolor, ballvelx, ballvely, ballr, hs, hr
+    if(ballx + ballvelx + ballr <= hr and ballx + ballvelx - ballr >= hs):
+        ballx += ballvelx
+    elif(ballx + ballvelx + ballr > hr):
+        ballx = ballx - ballvelx // (hr - ballx)
+        ballvelx = - ballvelx
+        changecolor()
+    else:
+        ballx = ballx - ballvelx // (ballx - hs)
+        ballvelx = - ballvelx
+        changecolor()
+    if(bally + ballvely + ballr <= hr and bally + ballvely - ballr >= hs):
+        bally += ballvely
+    elif(bally + ballvely + ballr > hr):
+        bally = bally - ballvely // (hr - bally)
+        ballvely = - ballvely
+        changecolor()
+    else :
+        bally = bally - ballvely // (bally - hs)
+        ballvely = - ballvely
+        changecolor()
+    if(bally < hr - ballr or ballvely != 0):
+        ballvely += 0.1
+
+    penColor(ballcolor)
+    brushColor(ballcolor)
+    circle(ballx, bally, ballr)
+
+def draw():
+    drawwall()
+    drawball()
+
+onTimer(draw, 10)
+run()
